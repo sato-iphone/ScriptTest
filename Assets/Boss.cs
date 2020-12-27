b3cb619305cf3aab88dc1edbf0dff7247e7f4fda
sato-iphone <75438572+sato-iphone@users.noreply.github.com>
2020-12-27 23:31:28 +0900

    Initial commit

---
+++ Assets/Boss.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Boss : MonoBehaviour
+{
+    private int mp = 53;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Magic()
+    {
+        if (mp >= 5)
+        {
+            mp -= 5;
+            Debug.Log("魔法攻撃をした。残りMPは" + mp.ToString() + "。");
+
+        }
+        else
+        {
+            Debug.Log("MPが足りないため魔法が使えない。");
+        }
+    }
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // declare and init array
+        int[] points = new int[5] { 1, 3, 5, 7, 9 };
+
+        // display all elements
+        for (int i = 0; i < 5; i++)
+        {
+            Debug.Log(points[i]);
+        }
+
+        // display all elements rev
+        for (int i = 0; i < 5; i++)
+        {
+            Debug.Log(points[points.Length - 1 - i]);
+        }
+
+        // MonoBehaviourクラスはnewできないらしいので
+        Boss firstBoss = gameObject.AddComponent<Boss>();
+
+        // Magicを10回実行
+        for (int i = 0; i < 10; i++)
+        {
+            firstBoss.Magic();
+        }
+        // Magicの実行11回目
+        firstBoss.Magic();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
